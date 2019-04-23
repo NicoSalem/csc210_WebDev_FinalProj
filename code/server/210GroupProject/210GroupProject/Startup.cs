9f257b220a598ba9885c92fc33089a3614da20af
Salih Zain <szainula@u.rochester.edu>
2019-04-23 17:07:08 -0400

    project

--- code/server/210GroupProject/210GroupProject/Startup.cs
+++ code/server/210GroupProject/210GroupProject/Startup.cs
@@ -68,7 +68,6 @@ namespace _210GroupProject
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
             });
 
             app.UseMvc();
